effc9d59c665b6088cf1ac8c856cc08a7338944d
Alex Beauchemin <abeauchemin@sweetiq.com>
2018-02-02 08:34:48 -0500

    Add tests for individual exchanges

---
+++ test/exchanges/binance.js
@@ -0,0 +1,30 @@
+import * as assert from 'assert';
+import get from 'lodash/get';
+import isEmpty from 'lodash/isEmpty';
+import binance from '../../dist/exchanges/binance';
+
+describe('Retrieving binance data', () => {
+  let data = [];
+
+  before(async () => {
+    try {
+      data = await binance.fetchData(null);
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+  describe('Fetch response', () => {
+    it('should return data', () => {
+      assert.equal(isEmpty(data), false);
+    });
+
+    it('should be an array of string', () => {
+      assert.equal(typeof(get(data, '[0]')), 'string');
+    });
+
+    it('should not be pairings', () => {
+      assert.equal(get(data, '[0]').indexOf('-'), -1);
+    });
+  });
+});
